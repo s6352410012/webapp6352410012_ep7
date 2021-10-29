a841f4d5edabc20a7aeea42c8273b1d525d2502c
s6352410012 <s6352410012@sau.ac.th>
2021-10-29 13:31:51 +0700

    Code V.1 Do: bahhh

---
+++ js/script2.js
@@ -0,0 +1,37 @@
+console.log("aaabbb")
+
+console.log('cccddd')
+
+console.log(1997)
+
+console.log(162.3)
+
+console.log("aaa'bbb")
+
+///console.log('ccc'ddd') error
+
+console.log('ccc\'ddd')
+
+
+//console.log("aaa"bbb") error
+console.log("aaa\"bbb")
+
+console.log('ccc"ddd')
+
+console.log(true)
+console.log(false)
+
+console.log(5 + 6)
+console.log(5 - 6)
+console.log(5 * 6)
+
+console.log('5' + '6')
+console.log('5' - '6')
+console.log('5' * '6')
+console.log("5" * "6")
+
+//console.log("abc" * "xyz")  ถ้าเป็น + จะไม่ error NaN = NOT A NUMBER
+console.log("abc" + "xyz")
+
+console.log(8 ** 3 ) //เครื่องหมายยกกำลัง **
+console.log(9 % 4) //เครื่องหมายหารเอาเศษ %
